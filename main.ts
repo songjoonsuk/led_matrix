21caa4dbe5e56c106389fd506bf06b041264c79b
songjoonsuk <52555877+songjoonsuk@users.noreply.github.com>
2020-09-03 16:37:23 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
 namespace led_matrix {
+
+    /**
+     * each LED lights on one by one 
+     * 
+     */
+    //% blockId=LED_MATRIX
+    //% block="LED on at indexed plot"
+    //% index.min=0 index.max=25
+
     export function plotN(index: number): void {
         index |= 0;
         const y = Math.floor(index / 5);
